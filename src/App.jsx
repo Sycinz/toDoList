c336f77463818effdc56799e7e242ac7db905f8f
Sycinz <gronbosof@gmail.com>
2023-11-16 11:53:29 +0100

    Added basic styling and website design concept

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ function App() {
   return (
   <main>
     {/* ToDo list on the left side */}
-    <section className='toDo-list'>
+    <section className='list'>
 
     </section>
     {/* ToDo list of content */}
-    <section className='toDo-content'>
+    <section className='content'>
 
     </section>
   </main>
